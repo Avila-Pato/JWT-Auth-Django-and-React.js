09395e04306a3d1a59cc51ce74cd2198b1fef33d
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-04 13:31:32 -0300

    El código define una vista en Django que permite a los usuarios registrarse mediante una API. A continuación, te explico cada parte clave

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+# El código define una vista en Django que permite a los usuarios
+# registrarse mediante una API. A continuación, te explico cada parte clave
+
+class UserRegistrationAPIViews(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request,*args, **kwargs):
+        serializers = self.get_serializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializers.data
+        data["tokens"] = {"refresh":str(token),
+                          "access": str(token.access_token)}
+        return Response(data, status= status.HTTP_201_CREATED)
+    
