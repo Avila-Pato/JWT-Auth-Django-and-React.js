f2873a0a8d2f869190644ce0c486f5ede1ff3d68
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-04 01:47:19 -0300

    Implement custom user model and admin integration

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class CustomUser(AbstractUser): 
+class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD="email"
     REQUIRED_FIELDS=["username"]
 
-    def __str__(self) -> str: 
+    def __str__(self) -> str:
         return self.email
