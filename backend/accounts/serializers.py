c3ea8b619c4046ac016b910368458a05ceb46645
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-04 12:46:13 -0300

    Add serializers for user registration and authentication with JWT support

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,33 @@
+from .models import CustomUser
+from rest_framework import serializers
+
+class CustomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ("id", "username", "email")
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ("id", "username", "email", "password1", "password2")
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError("Passwords do not match!")
+
+        password = attrs.get("password1", "")
+        if len(password) < 8:
+            raise serializers.ValidationError(
+                "Password must be at least 8 characters long!"
+            )
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop("password1")
+        validated_data.pop("password2")
+        return CustomUser.objects.create_user(password=password, **validated_data)
+
+# Define serializeer login
